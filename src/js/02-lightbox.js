131fbdff467dc0dcd4163be444db5eec395557da
Hanna Kostiana <anna.kostianaia@gmail.com>
2022-11-16 15:20:45 +0200

    task 1

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -3,30 +3,39 @@ import { galleryItems } from './gallery-items.js';
 
 const itemContainer = document.querySelector(`.gallery`);
 
+
+
 function createGalleryItemMarkup(galleryItems) {
     return galleryItems
         .map(({ preview, original, description }) => {
-        return `
-        <a class="gallery__item" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
+            return `
+        <li>
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
         `;
         })
         .join('');
 }
+
 const itemMarkup = createGalleryItemMarkup(galleryItems);
 
 itemContainer.insertAdjacentHTML('beforeend', itemMarkup);
 
 console.log(galleryItems);
 
-itemContainer.addEventListener('click', (evt) => {
-    evt.preventDefault();
-});
+// itemContainer.addEventListener('click', (evt) => {
+//     evt.preventDefault();
+// });
 
 var lightbox = new SimpleLightbox('.gallery a', { 
     captionsData: 'alt',
     captionDelay: 250,
+    captionPosition: 'bottom',
 });
 
+lightbox.on('show.simplelightbox', function () {
+	evt.preventDefault();
+});
 
